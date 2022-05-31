87c68922d549c6765cf6e48fa46440357333e16d
cogdiver <valentina.arenas2@gmail.com>
2022-05-31 00:16:41 -0500

    add plugin for fetch to apis

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ npm install webpack webpack-cli webpack-dev-server
 npm install babel-loader html-loader html-webpack-plugin
 npm install mini-css-extract-plugin css-loader style-loader sass sass-loader -D
 npm install react-router-dom
+npm install styled-components
+npm install @babel/plugin-transform-runtime
