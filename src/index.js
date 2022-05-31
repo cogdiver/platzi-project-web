ab22ae543a5202fd3182cbb9ab526840762b9016
cogdiver <valentina.arenas2@gmail.com>
2022-05-31 00:28:54 -0500

    change id in html and render for App

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { App } from './routes/App'
 import './styles/global.scss';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const root = createRoot(document.getElementById('root'))
+root.render( <App /> );
